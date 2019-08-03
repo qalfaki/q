bfe07ddbb3cdb36a252a6582a2ec397348006b3f
syntaxsizer <qusai.alfaki@gmail.com>
2019-08-03 16:58:05 +0300

    add additional resources and components including resume page

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Talks from './components/Talks';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+
+export default [
+	{
+		path: '/',
+		component: Home,
+		exact: true
+
+	},
+	{
+		path: '/skills',
+		component: Skills,
+		exact: true
+
+	},
+	{
+		path: '/contact',
+		component: Contact,
+		exact: true
+	},
+	{
+		path: '/portfolio',
+		component: Portfolio,
+		exact: true
+	},
+	{
+		path: '/talks',
+		component: Talks,
+		exact: true
+	},
+	{
+		path: '/resume',
+		component: Resume,
+		exact: true
+	},
+]
